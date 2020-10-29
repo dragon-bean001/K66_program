92639afbccd2d44cea1b20b8de2922e80d90dab1
dragon-bean001 <191347294@qq.com>
2020-10-29 21:10:02 +0800

    使用正反转，但是一直抖，加入五向开关，未完成

--- 电机驱动+电感/逐飞科技_K66库/Libraries/hareware/key/KEY.C
+++ 电机驱动+电感/逐飞科技_K66库/Libraries/hareware/key/KEY.C
@@ -1,15 +1,19 @@
 #include "headfile.h"
+#include "key.h"
+uint8 key_interrupt_flag;//按键中断标志位
+uint8 key_value_last;//上一个键值
+uint8 key_value;//当前键值
 
 void Key_init(void)//按键初始化
 {
-	port_init (D1, IRQ_EITHER | PF | ALT1 | PULLUP );	//初始化 D1 管脚，下降沿触发中断，带无源滤波器，复用功能为GPIO并设置为输入 ，上拉电阻
+	port_init (D0, IRQ_EITHER | PF | ALT1 | PULLUP );	//初始化 D1 管脚，下降沿触发中断，带无源滤波器，复用功能为GPIO并设置为输入 ，上拉电阻
+	port_init (D1, IRQ_EITHER | PF | ALT1 | PULLUP );
 	port_init (D2, IRQ_EITHER | PF | ALT1 | PULLUP );
-	port_init (D3, IRQ_EITHER | PF | ALT1 | PULLUP );
 	set_irq_priority(PORTD_IRQn,2);						//设置优先级
 	enable_irq(PORTD_IRQn);								//打开PORTD中断开关
 	EnableInterrupts;									//打开总的中断开关
 
-	Key_value=NONE;
-	Screen_num=1;
-	Key_interrupt_flag=0;//按键中断标志位，在多个文件传递
+	key_value=7;//啥都不按的情况
+	
+	key_interrupt_flag=0;//按键中断标志位，在多个文件传递
 }
